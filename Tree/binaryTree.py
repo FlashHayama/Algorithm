59299f8e210f31b4dbe8cfbd940c543602dfdcf8
Marchand Valentin <marchandvalentin.7@outlook.fr>
2021-10-08 11:42:41 +0200

    com 2

--- Tree/binaryTree.py
+++ Tree/binaryTree.py
@@ -13,6 +13,30 @@ class SearchBinaryTree(BinaryTree):
         self.root = root
     
     def append(self,data):
-        def append(self,data,tree):
-            pass
+        def append(data,tree):
+            if tree is None:
+                tree = Node(data)
+            elif tree.data < data:
+                append(data,tree.right)
+            elif tree.data > data:
+                append(data,tree.left)
+            else:
+                print("Data is already in tree")
         append(data,self.root)
+        print("Data appended")
+
+    def print_tree(self,tree):
+        if tree is None:
+            return
+        else:
+            self.print_tree()
+
+    def __str__(self):
+        pass
+
+tree = SearchBinaryTree()
+
+tree.append(56)
+tree.append(40)
+tree.append(100)
+tree.append(40)
