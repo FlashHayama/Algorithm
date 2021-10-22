a0d66f4d8bddf3a32db35671e4cf0bc727c0407f
valentin marchand <MarchandValentin.7@outlook.fr>
2021-10-22 16:17:18 +0200

    add test c++

---
+++ cpp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **)
+{
+    int x;
+
+    //std::cout << "Hello world" << endl << "salut";
+    //std::cout << "12" << endl;
+    cout << "Entrer une valeur : ";
+
+    cin >> x;
+
+    cout << "La valeur entrée est : " << x << endl;
+
+    return 0;
+}
