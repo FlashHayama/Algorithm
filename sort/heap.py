4974b130be76c23f28df21a6f946662dad5ac22b
Marchand Valentin <marchandvalentin.7@outlook.fr>
2021-10-28 14:51:57 +0200

    Add void method for heap

--- sort/heap.py
+++ sort/heap.py
@@ -1,12 +1,27 @@
 class Heap:
-    def __init__(self):
+    def __init__(self, tab = []):
+        self.tab = tab
+
+    def get_max(self):
         pass
 
-    def create_heap_with_insert(tab):
+    def get_min(self):
         pass
 
-    def create_heap(tab):
+    def delete(self,data):
         pass
 
-    def sort(tab,withInsert = False):
+    def append(self,data):
+        pass
+
+    def create_heap_with_insert(self):
+        pass
+
+    def create_heap(self):
+        pass
+
+    def sort(self,withInsert = False):
+        pass
+
+    def __str__(self):
         pass
