2e21a4c46552aed0567a46141187c6992c10ba2d
Marchand Valentin <marchandvalentin.7@outlook.fr>
2021-10-28 13:25:38 +0200

    start sort with heat

---
+++ sort/heap.py
@@ -0,0 +1,12 @@
+class Heap:
+    def __init__(self):
+        pass
+
+    def create_heap_with_insert(tab):
+        pass
+
+    def create_heap(tab):
+        pass
+
+    def sort(tab,withInsert = False):
+        pass
